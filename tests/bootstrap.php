937a6e61a0aa4a936499c1dc897f3325f676ad39
mscherer <mark.scherer@spryker.com>
2022-12-26 23:18:46 +0100

    Enable new schema.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -92,3 +92,8 @@ ConnectionManager::setConfig('test', [
 //use Migrations\TestSuite\Migrator;
 //$migrator = new Migrator();
 //$migrator->run();
+
+if (env('FIXTURE_SCHEMA_METADATA')) {
+	$loader = new Cake\TestSuite\Fixture\SchemaLoader();
+	$loader->loadInternalFile(env('FIXTURE_SCHEMA_METADATA'));
+}
