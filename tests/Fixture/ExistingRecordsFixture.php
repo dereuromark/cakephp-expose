286e287152bcae572a5b4bf3e42ac37c6c63be09
mscherer <dereuromark@web.de>
2023-11-13 23:49:37 +0100

    Fix tests

--- tests/Fixture/ExistingRecordsFixture.php
+++ tests/Fixture/ExistingRecordsFixture.php
@@ -20,7 +20,7 @@ class ExistingRecordsFixture extends TestFixture {
 		'modified' => ['type' => 'datetime', 'length' => null, 'precision' => null, 'null' => false, 'default' => null, 'comment' => ''],
 		'_constraints' => [
 			'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
-			'uuid' => ['type' => 'unique', 'columns' => ['uuid'], 'length' => []],
+			'records-uuid' => ['type' => 'unique', 'columns' => ['uuid'], 'length' => []],
 		],
 		'_options' => [
 			'engine' => 'InnoDB',
